1447ade4b5373b62a1c5cb62606f811b093ce4cf
Payal17122000 <raviyapayal17@gmail.com>
2024-02-26 11:08:13 +0530

    fix: add code for how to access sytem variables in project

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,6 +3,12 @@
 // import { useUsersStore } from "~/store/users";
 // const UsersStore = useUsersStore();
 // //console.log(UsersStore);
+
+/* To access environment variables defined within a composable */
+const systemEnv = useSystemEnv()
+console.log(systemEnv.value.base_url)
+
+
 </script>
 <template>
   <div class="text-center homepage">Welcome to nuxt3</div>
